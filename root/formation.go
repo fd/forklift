54b99e5b2479001b963c3283a7b20fdf159729f6
Simon Menke <simon.menke@gmail.com>
2013-07-03 16:13:38 +0200

    Better formation monitoring

--- root/formation.go
+++ root/formation.go
@@ -53,7 +53,10 @@ func (cmd *Root) BreakFormation() error {
 	cmd.Formation = data
 
 	if !cmd.DryRun {
-		time.Sleep(12 * time.Second)
+		err := cmd.wait_until_dynos_are_down()
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -62,8 +65,11 @@ func (cmd *Root) BreakFormation() error {
 func (cmd *Root) RestoreFormation() error {
 	fmt.Println("Restore formation:")
 	tabw := tabwriter.NewWriter(os.Stdout, 8, 8, 1, ' ', 0)
+	count := 0
 
 	for _, formation := range cmd.Formation {
+		count += int(formation.Quantity)
+
 		if formation.Quantity == 0 {
 			fmt.Fprintf(tabw, " - %s\tquantity=%d\tsize=%d\n",
 				formation.Type, formation.Quantity, formation.Size)
@@ -83,8 +89,96 @@ func (cmd *Root) RestoreFormation() error {
 	tabw.Flush()
 
 	if !cmd.DryRun {
-		time.Sleep(12 * time.Second)
+		err := cmd.wait_until_dynos_are_up(count)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
+}
+
+func (cmd *Root) wait_until_dynos_are_down() error {
+	deadline := time.Now().Add(5 * time.Minute)
+
+	for time.Now().Before(deadline) {
+		ok, err := cmd.are_all_dynos_down()
+
+		if err != nil {
+			time.Sleep(10 * time.Second)
+			continue
+		}
+
+		if ok {
+			return nil
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	return fmt.Errorf("formation: unable to stop dynos")
+}
+
+func (cmd *Root) wait_until_dynos_are_up(count int) error {
+	deadline := time.Now().Add(5 * time.Minute)
+
+	for time.Now().Before(deadline) {
+		ok, err := cmd.are_all_dynos_up(count)
+
+		if err != nil {
+			time.Sleep(10 * time.Second)
+			continue
+		}
+
+		if ok {
+			return nil
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	return fmt.Errorf("formation: unable to start dynos")
+}
+
+func (cmd *Root) are_all_dynos_down() (bool, error) {
+	var (
+		data []struct {
+			State string `json:"state"`
+		}
+	)
+
+	err := cmd.Http("GET", nil, &data, "/apps/%s/dynos", cmd.Config.Name)
+	if err != nil {
+		return false, err
+	}
+
+	if len(data) == 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (cmd *Root) are_all_dynos_up(count int) (bool, error) {
+	var (
+		data []struct {
+			AttachUrl *string `json:"attach_url"`
+			State     string  `json:"state"`
+		}
+	)
+
+	err := cmd.Http("GET", nil, &data, "/apps/%s/dynos", cmd.Config.Name)
+	if err != nil {
+		return false, err
+	}
+
+	for _, dyno := range data {
+		if dyno.AttachUrl == nil {
+			if dyno.State == "up" {
+				count--
+			}
+		}
+	}
+
+	return (count == 0), nil
 }

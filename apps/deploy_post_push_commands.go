cfd6b755350365c52176ae7fffb1df4386c511c9
Simon Menke <simon.menke@gmail.com>
2015-12-11 17:10:55 +0100

    Using heroku command for now

--- apps/deploy_post_push_commands.go
+++ apps/deploy_post_push_commands.go
@@ -1,12 +1,9 @@
 package apps
 
 import (
-	"bufio"
-	"crypto/tls"
 	"fmt"
-	"io"
-	"net/url"
 	"os"
+	"os/exec"
 )
 
 func (app *App) run_post_push_commands() error {
@@ -25,73 +22,19 @@ func (app *App) run_post_push_command(command string) error {
 		return nil
 	}
 
-	fmt.Printf("Run: %s\n", command)
-
-	var (
-		req = struct {
-			Command string            `json:"command"`
-			Attach  bool              `json:"attach"`
-			Env     map[string]string `json:"env"`
-		}{
-			Command: command,
-			Attach:  true,
-			Env: map[string]string{
-				"COLUMNS": "128",
-				"ROWS":    "50",
-			},
-		}
-
-		resp struct {
-			AttachURL string `json:"attach_url"`
-		}
-	)
-
-	err := app.HttpV3("POST", &req, &resp, "/apps/%s/dynos", app.AppName)
+	cmd := exec.Command("heroku", "run", command)
+	cmd.Env = append(
+		os.Environ(),
+		[]string{
+			"HEROKU_APP=" + app.AppName,
+		}...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = "/"
+	err := cmd.Run()
 	if err != nil {
 		fmt.Printf("error=%s", err)
-		return err
-	}
-
-	err = do_rendervous(resp.AttachURL)
-	if err != nil {
-		fmt.Printf("error=%s", err)
-		return err
-	}
-
-	return nil
-}
-
-func do_rendervous(rendezvous_url string) error {
-	u, err := url.Parse(rendezvous_url)
-	if err != nil {
-		return err
-	}
-
-	cn, err := tls.Dial("tcp", u.Host, nil)
-	if err != nil {
-		return err
-	}
-	defer cn.Close()
-
-	br := bufio.NewReader(cn)
-
-	_, err = io.WriteString(cn, u.Path[1:]+"\r\n")
-	if err != nil {
-		return err
-	}
-
-	for {
-		_, pre, err := br.ReadLine()
-		if err != nil {
-			return err
-		}
-		if !pre {
-			break
-		}
-	}
-
-	_, err = io.Copy(os.Stdout, br)
-	if err != nil {
 		return err
 	}
 
